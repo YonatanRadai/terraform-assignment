05ab8da04906afa7f61f699b3ac0994570a31c27
יונתן רדעי <radai2510@gmail.com>
2025-02-09 12:15:19 +0200

    added alb v2

--- modules/autoscaling/main.tf
+++ modules/autoscaling/main.tf
@@ -1,8 +1,6 @@
-resource "aws_autoscaling_group" "asg" {
-    desired_capacity = var.desired_capacity
-    max_size = var.max_size
-    min_size = var.min_size
-    vpc_zone_identifier = var.vpc_zone_identifier
-    
-  
-}
+# resource "aws_autoscaling_group" "asg" {
+#     desired_capacity = var.desired_capacity
+#     max_size = var.max_size
+#     min_size = var.min_size
+#     vpc_zone_identifier = var.vpc_zone_identifier
+# }
--- modules/autoscaling/variables.tf
+++ modules/autoscaling/variables.tf
@@ -1,16 +1,16 @@
-variable "desired_capacity" {
-    description = "desired capacity for asg"
-    default = 1
-}
-variable "max_size" {
-    description = "max size for asg"
-    default = 3
-}
-variable "min_size" {
-  description = "min size of asg"  
-  default = 1
-}
+# variable "desired_capacity" {
+#     description = "desired capacity for asg"
+#     default = 1
+# }
+# variable "max_size" {
+#     description = "max size for asg"
+#     default = 3
+# }
+# variable "min_size" {
+#   description = "min size of asg"  
+#   default = 1
+# }
 
-variable "vpc_zone_identifier" {
-    description = "id of availability zones"
-}
+# variable "vpc_zone_identifier" {
+#     description = "id of availability zones"
+# }
